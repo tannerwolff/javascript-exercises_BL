e9d4f3dd81605fbebeb29e8ead69e87a526939f0
tannerwolff <tawolff13@gmail.com>
2026-06-16 13:53:07 -0700

    Complete JS Exercise 11_tempConversions
    Finish each function with the temp change
    formula and use .toFixed(1) to round to nearest 1
    decimal value.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+  return +((num - 32) * (5 / 9)).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  return +(num * (9 / 5) + 32).toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
