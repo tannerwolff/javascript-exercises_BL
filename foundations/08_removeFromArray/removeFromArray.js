0101569b535857ab6ae93328eba788a083def012
tannerwolff <tawolff13@gmail.com>
2026-06-16 12:29:21 -0700

    Complete JS Exercise 08_removeFromArray
    Add 2 arguments, arr and ...args to get the array,
    and then the numbers of arguments needing removal.
    Create a new array and push the items to it IF the
    items are not in the ...args list using
    !args.includes(item)

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  const newArr = [];
+  arr.forEach((item) => {
+    if (!args.includes(item)) {
+      newArr.push(item);
+    }
+  });
+  return newArr;
 };
 
 // Do not edit below this line
