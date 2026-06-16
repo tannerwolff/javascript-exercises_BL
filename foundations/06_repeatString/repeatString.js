721583e71a4b0ceeea8618f7965a9aba019a830f
tannerwolff <tawolff13@gmail.com>
2026-06-16 11:52:40 -0700

    Complete JS exercise 06_repeatString
    Use for loop to run "times" number of times,
    adding the string to the end of a variable 'final'
    in order to repeat the string "times" times and
    return the fully appended string.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, times) {
+  if (times < 0) return "ERROR";
+  let final = "";
+  for (let i = 0; i < times; i++) {
+    final += str;
+  }
+  return final;
 };
 
 // Do not edit below this line
