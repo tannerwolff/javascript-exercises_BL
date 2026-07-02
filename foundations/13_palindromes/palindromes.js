bff5d6f14a26d8fd0e8f50167dc5c1c885ec1952
tannerwolff <tawolff13@gmail.com>
2026-07-02 15:41:17 -0700

    Complete Exercise 13_palindromes
    Clean string by replacing non-alphanumeric chars
    with nothing using a regex. Modify the string with
    .split("").reverse().join("") to flip the clean
    string and then test the reversed with the clean
    and return the result.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const reg = /[^a-z0-9]/g;
+  let cleaned = str.toLowerCase().replace(reg, "");
+  let mods = cleaned.split("").reverse().join("");
+  if (mods === cleaned) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
